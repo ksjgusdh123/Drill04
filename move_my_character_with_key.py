1df94aa61464dc80d33ed0b6ace9b9d428cdd16a
hyundojeong <122209567+ksjgusdh123@users.noreply.github.com>
2023-09-25 18:28:33 +0900

    show character

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,12 +1,30 @@
 from pico2d import *
 
 open_canvas()
-character = load_image('sonic2.png')
+character = load_image('sonic3.png')
 ground = load_image('TUK_GROUND.png')
 
 
 def handle_events():
-    pass
+    global dir, running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.type == SDLK_RIGHT:
+                dir += 1
+            elif event.type == SDLK_LEFT:
+                dir -= 1
+            elif event.type == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.type == SDLK_RIGHT:
+                dir -= 1
+            elif event.type == SDLK_LEFT:
+                dir += 1
+
 
 running = True
 x = 800 // 2
@@ -15,4 +33,12 @@ dir = 0
 
 
 while running:
-    pass
+    clear_canvas()
+    ground.draw(800 // 2,600 // 2)
+    character.clip_draw(173, 345, 45, 100, x, 100)
+    update_canvas()
+    handle_events()
+    x += dir * 5
+    delay(0.05)
+
+close_canvas()
