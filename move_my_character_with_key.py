d146731747f002d187dc72af92226a776e4bfb22
hyundojeong <122209567+ksjgusdh123@users.noreply.github.com>
2023-09-25 18:13:23 +0900

    기본 틀 작성

---
+++ move_my_character_with_key.py
@@ -0,0 +1,18 @@
+from pico2d import *
+
+open_canvas()
+character = load_image('sonic2.png')
+ground = load_image('TUK_GROUND.png')
+
+
+def handle_events():
+    pass
+
+running = True
+x = 800 // 2
+frame = 0
+dir = 0
+
+
+while running:
+    pass
